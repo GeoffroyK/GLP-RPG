9969e4b3ea2cd210570f045a639a7ff7b1aa405f
Hang Alexandre <Hang Alexandre@DESKTOP-8R91MPH>
2020-02-17 22:33:43 +0100

    Creation Zukino branch and Spell class

--- RPG/src/dataclasses/DataBase.java
+++ RPG/src/dataclasses/DataBase.java
@@ -1,65 +1,88 @@
 package dataclasses;
 
+import java.io.BufferedReader;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
+
+import playable.Spell;
 
 public class DataBase {
 	
-	private HashMap<String,Equipment>equipments;
-	private HashMap<String,Consumable>consumables;
-	private HashMap<String,Character>characters;
-	private HashMap<String,Tile>tiles;
-	private HashMap<String,Prop>props;
+//	private HashMap<String,Equipment>equipments;
+//	private HashMap<String,Consumable>consumables;
+//	private HashMap<String,Character>characters;
+//	private HashMap<String,Tile>tiles;
+//	private HashMap<String,Prop>props;
+	private HashMap<String,Spell>spells;
 	
 	private String[]csvGameObjectPaths = {"csvConsumable","csvEquipment","csvSpell","csvTile","csvProp","csvCharacter"}	;
 	
 	public DataBase() {
-		loadCsv();
+		loadCsvSpell();
+		System.out.println(this);
 	}
 
-	private void loadCsv() {
+	private void loadCsvSpell() {
+		BufferedReader br = new BufferedReader()
 		
+	}
+	
+	public static void main(String[]args) {
+		 new DataBase();	
+	}
+	
+	public String toString() {
 		
+		Collection<Spell> vals = spells.values();
+		Iterator<Spell> it = vals.iterator();
+		String res="";
+		while(it.hasNext()) {
+			Spell spell = it.next();
+			res += spell.toString();
+		}
+		return res;
 	}
 
-	public HashMap<String, Equipment> getEquipments() {
-		return equipments;
-	}
-
-	public void setEquipments(HashMap<String, Equipment> equipments) {
-		this.equipments = equipments;
-	}
-
-	public HashMap<String, Consumable> getConsumables() {
-		return consumables;
-	}
-
-	public void setConsumables(HashMap<String, Consumable> consumables) {
-		this.consumables = consumables;
-	}
-
-	public HashMap<String, Character> getCharacters() {
-		return characters;
-	}
-
-	public void setCharacters(HashMap<String, Character> characters) {
-		this.characters = characters;
-	}
-
-	public HashMap<String, Tile> getTiles() {
-		return tiles;
-	}
-
-	public void setTiles(HashMap<String, Tile> tiles) {
-		this.tiles = tiles;
-	}
-
-	public HashMap<String, Prop> getProps() {
-		return props;
-	}
-
-	public void setProps(HashMap<String, Prop> props) {
-		this.props = props;
-	}
+//	public HashMap<String, Equipment> getEquipments() {
+//		return equipments;
+//	}
+//
+//	public void setEquipments(HashMap<String, Equipment> equipments) {
+//		this.equipments = equipments;
+//	}
+//
+//	public HashMap<String, Consumable> getConsumables() {
+//		return consumables;
+//	}
+//
+//	public void setConsumables(HashMap<String, Consumable> consumables) {
+//		this.consumables = consumables;
+//	}
+//
+//	public HashMap<String, Character> getCharacters() {
+//		return characters;
+//	}
+//
+//	public void setCharacters(HashMap<String, Character> characters) {
+//		this.characters = characters;
+//	}
+//
+//	public HashMap<String, Tile> getTiles() {
+//		return tiles;
+//	}
+//
+//	public void setTiles(HashMap<String, Tile> tiles) {
+//		this.tiles = tiles;
+//	}
+//
+//	public HashMap<String, Prop> getProps() {
+//		return props;
+//	}
+//
+//	public void setProps(HashMap<String, Prop> props) {
+//		this.props = props;
+//	}
 
 	public String[] getCsvGameObjectPaths() {
 		return csvGameObjectPaths;
