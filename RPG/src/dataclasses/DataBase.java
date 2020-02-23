0b0c8c16f173be8b62058cf4517211cfebd5dc49
Hang Alexandre <Hang Alexandre@DESKTOP-8R91MPH>
2020-02-23 21:43:16 +0100

    fix commit

--- RPG/src/dataclasses/DataBase.java
+++ RPG/src/dataclasses/DataBase.java
@@ -38,8 +38,9 @@ public class DataBase extends Canvas {
 	private HashMap<String,Equipment>equipments;
 	private static HashMap<String,Consumable>consumables;
 	private HashMap<String,GameObject> instances;
-
+	
 	private Scanner sc;
+
 
 	private String[] csvGameObjectPaths = { ".\\CSV\\Consumable.csv", ".\\CSV\\Equipment.csv", ".\\CSV\\Spell.csv", "csvTile", "csvProp",
 			".\\CSV\\Player.csv", ".\\CSV\\Monster.csv" };
@@ -59,12 +60,8 @@ public class DataBase extends Canvas {
 			loadCsvSpell();
 			loadCsvPlayer();
 			loadCsvMonster();
-
 			loadCsvConsumable();
 			loadCSVEquipments();
-			//loadCsvProp(); 
-			// loadCsvConsumable();
-			// loadCSVEquipments();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -107,55 +104,55 @@ public class DataBase extends Canvas {
 	}
 	
 	public void loadCsvSpell() throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(csvGameObjectPaths[2]));
-        br.readLine();
-
-        String line;
-        String[] spellFields;
-        int[] spellIntCSV = new int[5];
-        float[] spellFloatCSV = new float[2];
-
-        while ((line = br.readLine()) != null) {
-            spellFields = line.split(",");
-
-            spellIntCSV[0] = Integer.parseInt(spellFields[4]);
-            spellIntCSV[1] = Integer.parseInt(spellFields[5]);
-            spellIntCSV[2] = Integer.parseInt(spellFields[6]);
-            spellIntCSV[3] = Integer.parseInt(spellFields[7]);
-            spellIntCSV[4] = Integer.parseInt(spellFields[8]);
-            spellFloatCSV[0] = Float.parseFloat(spellFields[9]);
-            spellFloatCSV[1] = Float.parseFloat(spellFields[10]);
-            Spell tmp = new Spell(spellFields[0], spellFields[1], spellFields[2], spellFields[3], spellIntCSV[0],
-                    spellIntCSV[1], spellIntCSV[2], spellIntCSV[3], spellIntCSV[4], spellFloatCSV[0], spellFloatCSV[1]);
-            spells.put(tmp.getId(), tmp);
-        }
-
-        br.close();
-    }
-	
-	/*public void loadCsvProp() throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader(csvGameObjectPaths[4]));
+		BufferedReader br = new BufferedReader(new FileReader(csvGameObjectPaths[2]));
 		br.readLine();
 
 		String line;
-		String[] propFields;
-		int[] propIntCSV = new int[1];
-		String[] propStringCSV = new String[2];
+		String[] spellFields;
+		int[] spellIntCSV = new int[5];
+		float[] spellFloatCSV = new float[2];
 
 		while ((line = br.readLine()) != null) {
-			propFields = line.split(",");
-			propIntCSV[0] = Integer.parseInt(propFields[2]);
-			propStringCSV[0] = propFields[0];
-			propStringCSV[1] = propFields[1];
-			propStringCSV[2] = propFields[3];
-			Prop tmp = new Prop(propStringCSV[0], propStringCSV[1],
-					propIntCSV[0], propStringCSV[2]);
-			props.put(tmp.getId(), tmp);
+			spellFields = line.split(",");
+
+			spellIntCSV[0] = Integer.parseInt(spellFields[4]);
+			spellIntCSV[1] = Integer.parseInt(spellFields[5]);
+			spellIntCSV[2] = Integer.parseInt(spellFields[6]);
+			spellIntCSV[3] = Integer.parseInt(spellFields[7]);
+			spellIntCSV[4] = Integer.parseInt(spellFields[8]);
+			spellFloatCSV[0] = Float.parseFloat(spellFields[9]);
+			spellFloatCSV[1] = Float.parseFloat(spellFields[10]);
+			Spell tmp = new Spell(spellFields[0], spellFields[1], spellFields[2], spellFields[3], spellIntCSV[0],
+					spellIntCSV[1], spellIntCSV[2], spellIntCSV[3], spellIntCSV[4], spellFloatCSV[0], spellFloatCSV[1]);
+			spells.put(tmp.getId(), tmp);
 		}
 
 		br.close();
+	}
+	
+	/*public void loadCsvProp() throws IOException {
+	BufferedReader br = new BufferedReader(new FileReader(csvGameObjectPaths[4]));
+	br.readLine();
+
+	String line;
+	String[] propFields;
+	int[] propIntCSV = new int[1];
+	String[] propStringCSV = new String[2];
+
+	while ((line = br.readLine()) != null) {
+		propFields = line.split(",");
+		propIntCSV[0] = Integer.parseInt(propFields[2]);
+		propStringCSV[0] = propFields[0];
+		propStringCSV[1] = propFields[1];
+		propStringCSV[2] = propFields[3];
+		Prop tmp = new Prop(propStringCSV[0], propStringCSV[1],
+				propIntCSV[0], propStringCSV[2]);
+		props.put(tmp.getId(), tmp);
+	}
+
+	br.close();
 <<<<<<< HEAD
-	}*/
+}*/
 	
 	private void loadCsvConsumable() throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(csvGameObjectPaths[0]));
