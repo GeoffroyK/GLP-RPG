77cb4b6740c6e13d1d0cb1ec6eaf9a341caa9852
Hang Alexandre <Hang Alexandre@DESKTOP-8R91MPH>
2020-03-03 20:36:50 +0100

    timer

--- RPG/src/game/Main.java
+++ RPG/src/game/Main.java
@@ -1,10 +1,17 @@
 package game;
 
 import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_A;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_D;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_RIGHT;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_S;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_W;
+import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_1;
 import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
 import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
 import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
 import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetKey;
 import static org.lwjgl.glfw.GLFW.glfwInit;
 import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
 import static org.lwjgl.glfw.GLFW.glfwPollEvents;
@@ -12,6 +19,13 @@ import static org.lwjgl.glfw.GLFW.glfwShowWindow;
 import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
 import static org.lwjgl.glfw.GLFW.glfwWindowHint;
 import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+import static org.lwjgl.opengl.GL11.GL_TRUE;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glVertex2f;
 
 import java.util.Scanner;
 
@@ -31,6 +45,7 @@ public class Main {
 	private long window ;
 	private float x = 0 ;
 	private float y = 0 ;
+	int direction = 0 ;
 	
 	private boolean running = false;
 	
@@ -59,48 +74,48 @@ public class Main {
 	
 	public void start() {
 		running = true ;
-//		initGame();
 		tick() ;
 	}
-	
-//	private void initGame() {
-//		
-//		Monster ronflex = (Monster) DataBase.getCharacters().get("ma2");
-//		ronflex.setX(5);
-//		ronflex.setY(5);
-//		ronflex.setDirection(0);
-//		Monster monstre = (Monster) DataBase.getCharacters().get("mt3");
-//		monstre.setX(6);
-//		monstre.setY(5);
-//		monstre.setDirection(0);
-//		DataBase.getInstances().put(ronflex.getId(),ronflex);
-//		
-//		Prop coffre = new Prop("id","coffre",1,"null");
-//		coffre.setX(5);
-//		coffre.setY(0);
-//		DataBase.getInstances().put(coffre.getId(), coffre);
-//
-//
-//		sc = new Scanner(System.in);
-//		System.out.println("CHOOSE CLASS OF CHARACTER : 't' = WARRIOR / 'y' = ARCHER / 'u' = MAGE\n OR EXIT = 'e'");
-//			String input = sc.nextLine() ;
-//			PlayerChoice.chooseClassPlayer(input);
-//
-//		InventoryKey.addLoot(loots.get("E#001"), PlayerChoice.selected());
-//		InventoryKey.addLoot(loots.get("E#002"), PlayerChoice.selected());
-//		
-//		//SALE ENCULE RENOMME TA METHODE RUN OU TICK PAS PTN DE GAMEINPUT
-//		new GameInput();
-//		sc.close();
-//		
-//	}
+	 
 	
 	public void tick() {
+		
+		boolean can_render = false;
+		
+		double frame_time = 0;
+		int frames = 0;
+		
+		double frame_cap = 1.0 / 60.0; // 60 frames/sec
+		double time = Timer.getTime();
+		double unprocessed = 0;
+		
 		while(running) {
 			if(glfwWindowShouldClose(window)) stop() ;
-			glfwPollEvents();
-			render() ;
-			glfwSwapBuffers(window);
+			
+			double time_2 = Timer.getTime(); // Actual time in each loop
+			double passed= time_2 - time; // Time that has passed btw last loop
+			unprocessed += passed; // Time where game isn't processed yet
+			
+			frame_time += passed;
+			
+			time = time_2;
+			
+			while(unprocessed >= frame_cap) { 
+				unprocessed -= frame_cap;
+				can_render=true;
+				glfwPollEvents();
+				
+				if(frame_time >= 1.0) {
+					frame_time = 0;
+					System.out.println("FPS : " + frames );
+					frames = 0;
+				}
+			}
+			if(can_render) {
+				render() ;
+				glfwSwapBuffers(window);
+				frames++;
+			}
 		}
 		exit() ;
 	}
@@ -116,5 +131,42 @@ public class Main {
 	
 	public void render() {
 		
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		if(glfwGetKey(window, GLFW_KEY_W) == GL_TRUE) {
+			y += 0.001f ;
+			direction = 1 ;
+		}
+		if(glfwGetKey(window, GLFW_KEY_D)== GL_TRUE) {
+			x += 0.001f ;
+			direction = 2 ;
+		}
+		if(glfwGetKey(window, GLFW_KEY_A) == GL_TRUE) {
+			x -= 0.001f ;
+			direction = 3 ;
+		}
+		if(glfwGetKey(window, GLFW_KEY_S) == GL_TRUE) {
+			y -= 0.001f ;
+			direction = 4 ;
+		}
+		if(glfwGetKey(window, GLFW_MOUSE_BUTTON_1) == GL_TRUE) {
+			System.out.println("ceci est un souris listnerer");
+		}
+		if(glfwGetKey(window, GLFW_KEY_RIGHT) == GL_TRUE) {
+			glBegin(GL_QUADS);
+			glVertex2f(-0.01f, 0.01f);
+			glVertex2f(0.002f, 0.01f);
+			glVertex2f(0.002f, -0.01f);
+			glVertex2f(-0.01f, -0.01f);
+			glEnd();	
+		}
+		
+		glBegin(GL_QUADS);
+		glVertex2f(-0.01f+ x, 0.01f + y);
+		glVertex2f(0.002f+ x, 0.01f + y);
+		glVertex2f(0.002f+ x, -0.01f + y);
+		glVertex2f(-0.01f + x, -0.01f +y);
+		glEnd();
 	}
+	
 }
--- RPG/src/game/Timer.java
+++ RPG/src/game/Timer.java
@@ -3,6 +3,6 @@ package game;
 public class Timer {
 
 	public static double getTime() {
-		return (double)System.nanoTime() / (double) 1000000000L; // nanoseconde / 1 milliard pour des secondes
+		return (double)System.nanoTime() / (double) 1000000000; // nanoseconde / 1 milliard pour des secondes
 	}
 }
