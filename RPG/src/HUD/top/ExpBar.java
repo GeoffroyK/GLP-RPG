cbed573fe05f274df2569068f86faab970eb07b8
Vortex <Vortex@DESKTOP-T4827LF>
2020-03-25 01:18:18 +0100

    Baby

--- RPG/src/HUD/top/ExpBar.java
+++ RPG/src/HUD/top/ExpBar.java
@@ -7,13 +7,13 @@ import playable.Player;
 
 public class ExpBar {
 	public void BarRender(Player hero, Graphics g) {
-		g.setColor(Color.GRAY);
-		g.fillRect(60, 47, 100, 15);
+		/*g.setColor(Color.GRAY);
+		g.fillRect(60, 47, 100, 15);*/
 		
 		float p = ((float) hero.getLifePoint()/(float) hero.getLifePointMax())*100 ;
 		
 		g.setColor(Color.GREEN);
-		g.fillRect(60, 47, (int) p , 15);
+		g.fillRect(71, 53, (int) p , 15);
 	}
 
 }
--- RPG/src/HUD/top/MpBar.java
+++ RPG/src/HUD/top/MpBar.java
@@ -8,13 +8,10 @@ import playable.Player;
 public class MpBar {
 	
 	public void BarRender(Player hero, Graphics g) {
-		g.setColor(Color.GRAY);
-		g.fillRect(60, 26, 100, 15);
-		
 		float p = ((float) hero.getManaPoint()/(float) hero.getManaPointMax())*100 ;
 		
 		g.setColor(Color.BLUE);
-		g.fillRect(60, 26, (int) p , 15);
+		g.fillRect(71, 32, (int) p , 15);
 	}
 
 }
